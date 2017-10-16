f8ae4b03eaf9e54b2b0c4df99675cfb9e2cb02b0
Bui Do Hiep <buidohiep@gmail.com>
2017-10-16 21:54:25 +0800

    Fix remote.sh to load db first

--- remote.sh
+++ remote.sh
@@ -4,11 +4,12 @@ for nc in 10 20 40
 do
     for cl in "ONE" "QUORUM"
     do
-        ssh xcnd50 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash run.sh 1 $nc $cl" &
-        ssh xcnd51 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash run.sh 2 $nc $cl" &
-        ssh xcnd52 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash run.sh 3 $nc $cl" &
-        ssh xcnd53 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash run.sh 4 $nc $cl" &
-        ssh xcnd54 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash run.sh 5 $nc $cl" &
+        ssh xcnd50 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash load_data.sh /home/stuproj/cs4224k/CS4224-Cassandra/data/data-files > log/$nc-$cl-load-data.txt"
+        ssh xcnd50 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash main.sh 1 $nc $cl > log/1-$nc-$cl-log.txt" &
+        ssh xcnd51 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash main.sh 2 $nc $cl > log/2-$nc-$cl-log.txt" &
+        ssh xcnd52 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash main.sh 3 $nc $cl > log/3-$nc-$cl-log.txt" &
+        ssh xcnd53 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash main.sh 4 $nc $cl > log/4-$nc-$cl-log.txt" &
+        ssh xcnd54 "cd /home/stuproj/cs4224k/CS4224-Cassandra && bash main.sh 5 $nc $cl > log/5-$nc-$cl-log.txt" &
         wait
         echo "Done $nc $cl..."
     done
